2147541a4c40f234ac90ae7e84b4952e90d000dd
BrianJJordan <brianjordan@temple.edu>
2025-01-09 19:19:37 -0500

    file opens

--- convertor.c
+++ convertor.c
@@ -2,9 +2,27 @@
 #include <stdlib.h>
 
 // functions
+void read_file (int *filename){
 
+}
 
 int main(void){
+    FILE* fptr;
+
+    fptr = fopen("convertor.html", "w");
+
+    if (fptr == NULL){
+        puts("File is not opened\n");
+        exit(0);
+    }
+    else {
+        puts("File created successfully\n");
+    }
+
     
+
+
+    
+
     return 0;
 }
